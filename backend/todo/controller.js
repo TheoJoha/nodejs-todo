14be1bb5315654da5e7f4654c8663f06b9e90f1f
Theodor Johansson <t.j.u@telia.com>
2024-09-28 17:24:13 +0200

    implemented delete functionality

--- backend/todo/controller.js
+++ backend/todo/controller.js
@@ -1,6 +1,12 @@
-import {getAll} from "./model.js"
+import {getAll, remove} from "./model.js"
 
 export async function listAction(req,res) {
     const data = await getAll()
     res.send(data)
+}
+
+export async function removeAction(req, res) {
+    const id = parseInt(req.params.id, 10)
+    await remove(id)
+    res.redirect(req.baseUrl)
 }
--- backend/todo/model.js
+++ backend/todo/model.js
@@ -1,4 +1,4 @@
-const data = [
+let data = [
     {id: 1, name: "todo1", description: "...", time: 10},
     {id: 2, name: "todo2", description: "...", time: 20},
     {id: 3, name: "todo3", description: "...", time: 30}
@@ -6,4 +6,9 @@ const data = [
 
 export function getAll() {
     return Promise.resolve(data)
+}
+
+export function remove(id) {
+    data = data.filter(todo => todo.id !== id)
+    return Promise.resolve()
 }
